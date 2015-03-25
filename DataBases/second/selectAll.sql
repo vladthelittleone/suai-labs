ce80597767689336ba0a1fc8c839355edf244513
vladthelittleone <vladthelittleone@gmail.com>
2015-03-25 23:44:54 +0400

    Create selectAll.sql

---
+++ DataBases/second/selectAll.sql
@@ -0,0 +1,11 @@
+-- Лабораторная работа №2.
+-- Вариант №15 по дисциплине "Базы данных".
+-- Вывод набора данных, содержащихся в таблицах БД.
+-- СПБ ГУАП 2015.
+
+select * from audience
+select * from corps
+select * from class
+select * from faculty
+select * from uni_group
+go
